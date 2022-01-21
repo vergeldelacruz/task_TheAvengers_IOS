611680d54400967ba90075bcddf3f275f85f67cf
lcmaze <lcmazeweb@gmail.com>
2022-01-20 21:20:47 -0500

    developed the first page UI

---
+++ task_TheAvengers_IOS/controllers/HomeController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeController.swift
+//  task_TheAvengers_IOS
+//
+//  Created by Litson Thomas on 2022-01-20.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
