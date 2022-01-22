7abafcfc647df4e871f9df48090878821a2f0ad4
lcmaze <lcmazeweb@gmail.com>
2022-01-21 21:23:21 -0500

    added the categories page and the table view category cell xib as well

---
+++ task_TheAvengers_IOS/cells/category/CategoryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryTableViewCell.swift
+//  task_TheAvengers_IOS
+//
+//  Created by Litson Thomas on 2022-01-21.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
