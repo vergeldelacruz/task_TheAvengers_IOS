53270b78aeaf1402f3007f32a4c3e5f790772f90
Vergel dela Cruz <C0835208@mylambton.ca>
2022-01-22 23:05:25 -0500

    Implemented the add/edit/delete feature for category

--- task_TheAvengers_IOS/controllers/AddCategoryViewController.swift
+++ task_TheAvengers_IOS/controllers/AddCategoryViewController.swift
@@ -4,15 +4,19 @@
 //
 //  Created by Litson Thomas on 2022-01-21.
 //
+//  Updated by Vergel dela Cruz on 2022-01-22.
+//  Description: Add featured to add/edit/delete category
 
 import UIKit
 import CoreData
 
 class AddCategoryController: UIViewController {
-    @IBOutlet weak var txvCategoryName: UITextField!
- 
+
     @IBOutlet weak var tableView: UITableView!
-    var items : [Category]?
+    
+    @IBOutlet weak var txvCategoryName: UITextField!
+    //var items : [Category]?
+    var items = [Category]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
@@ -23,20 +27,33 @@ class AddCategoryController: UIViewController {
         //addCategory(name: "School")
         fetchCategories()
     }
+    
     @IBAction func onAddCategory(_ sender: Any) {
         let categoryName = txvCategoryName.text
-        if ((categoryName?.isEmpty) != nil) {
+        if categoryName != "" {
+            let catNames = self.items.map {$0.name?.lowercased()}
+            guard !catNames.contains(categoryName?.lowercased()) else {showAlert(title: "Add Category" , message: "This name already exists. Please choose another name."); return}
             addCategory(name: txvCategoryName.text ?? "")
             txvCategoryName.text = ""
             fetchCategories()
         } else {
-            print("Category name is empty")
+            showAlert(title:"Add Category", message: "Name can't be blank. Please enter another name.")
         }
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "Ok", style: .default) {
+            (action) in
+            print("Category name is blank.")
+        }
+        alert.addAction(okButton)
+        self.present(alert,animated: true,completion: nil)
     }
     func addCategory(name:String) {
         let newCategory = Category(context: self.context)
         newCategory.name = name
-            
+        print("Add " + name)
         do {
             try self.context.save()
         } catch {
@@ -44,6 +61,7 @@ class AddCategoryController: UIViewController {
         }
         tableView.reloadData()
     }
+    
     func fetchCategories() {
         do {
             self.items = try context.fetch(Category.fetchRequest())
@@ -59,19 +77,19 @@ class AddCategoryController: UIViewController {
 
 extension AddCategoryController : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.items?.count ?? 0
+        return self.items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell =     tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        let category =  self.items![indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let category =  self.items[indexPath.row]
         cell.textLabel?.text = category.name
         return cell
     }
     
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
         let action = UIContextualAction(style: .destructive, title: "Delete")     { action, view, completionHandler    in
-            let categoryToRemove = self.items![indexPath.row]
+            let categoryToRemove = self.items[indexPath.row]
             self.context.delete(categoryToRemove)
             do {
                 try self.context.save()
@@ -84,7 +102,7 @@ extension AddCategoryController : UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let category = self.items![indexPath.row]
+        let category = self.items[indexPath.row]
         let alert = UIAlertController(title: "Edit Category", message: "Edit Name", preferredStyle: .alert)
         alert.addTextField()
         let textField = alert.textFields![0]
@@ -93,6 +111,9 @@ extension AddCategoryController : UITableViewDelegate, UITableViewDataSource {
             (action) in
             let textField = alert.textFields![0]
             category.name = textField.text
+            //let catNames = self.items.map {$0.name?.lowercased()}
+            //guard !catNames.contains(textField.text?.lowercased()) else {self.showAlert(title:"Edit Category",message: "This name already exists. Please choose another name."); return}
+           
             do {
                 try self.context.save()
             } catch {
